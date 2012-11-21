10635335c9f8381ecfd4a13186142896de442803
Ajay Gopinath <ajgopi124@gmail.com>
2012-11-20 17:02:39 -0800

    initial commit, basic login UI finished

---
+++ src/com/cyanojay/schoolloopd/MainActivity.java
@@ -0,0 +1,27 @@
+package com.cyanojay.schoolloopd;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.example.schoolloopd.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void sendMessage(View view) {
+    	
+    }
+}
