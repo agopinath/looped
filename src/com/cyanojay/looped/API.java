3078fd6431881b80cbaec325c08af958596ba744
Ajay Gopinath <ajgopi124@gmail.com>
2012-11-22 11:05:17 -0800

    added singleton behavior to API

--- src/com/cyanojay/looped/API.java
+++ src/com/cyanojay/looped/API.java
@@ -1,5 +1,12 @@
 package com.cyanojay.looped;
 
 public class API {
+	private static API instance;
 	
+	// instantiation is prevented
+	private API() {}
+	
+	public static API get() {
+		return (instance != null) ? instance : (instance = new API());
+	}
 }
