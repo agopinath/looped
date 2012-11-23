118fdee01a5e9f056fc9cc8e236df22e2ed69c5f
Ajay Gopinath <ajgopi124@gmail.com>
2012-11-22 17:29:23 -0800

    finished proper formatting of table

--- src/com/cyanojay/looped/portal/GradesActivity.java
+++ src/com/cyanojay/looped/portal/GradesActivity.java
@@ -38,6 +38,29 @@ public class GradesActivity extends Activity {
 	        super.onPostExecute(result);
 	        TableLayout table = (TableLayout) findViewById(R.id.grades_table);
 	        
+	        TableRow headers = new TableRow(GradesActivity.this);
+	        
+	        TextView subj_head = new TextView(GradesActivity.this);
+	        TextView lett_head = new TextView(GradesActivity.this);
+	        TextView pct_head = new TextView(GradesActivity.this);
+	        TextView zero_head = new TextView(GradesActivity.this);
+	        
+	        subj_head.setTextSize(20.0f);
+	        subj_head.setText("  Course Name  \n");
+	        lett_head.setTextSize(20.0f);
+	        lett_head.setText("  Letter Grade  \n");
+	        pct_head.setTextSize(20.0f);
+	        pct_head.setText("  % Grade  \n");
+	        zero_head.setTextSize(20.0f);
+	        zero_head.setText("  # of Zeroes  \n");
+	        
+	        headers.addView(subj_head);
+	        headers.addView(lett_head);
+	        headers.addView(pct_head);
+	        headers.addView(zero_head);
+	        
+	        table.addView(headers, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+	        
 	        for(Course currCourse: result) {
 		        TableRow row = new TableRow(GradesActivity.this);
 		         
@@ -46,17 +69,22 @@ public class GradesActivity extends Activity {
 		        TextView pctGrade = new TextView(GradesActivity.this);
 		        TextView numZero = new TextView(GradesActivity.this);
 		        
-		        subject.setText(currCourse.getName());
-		        lettGrade.setText(currCourse.getLetterGrade());
-		        pctGrade.setText(currCourse.getPercentGrade());
-		        numZero.setText(""+currCourse.getNumZeros());
+		        subject.setTextSize(18.0f);
+		        lettGrade.setTextSize(18.0f);
+		        pctGrade.setTextSize(18.0f);
+		        numZero.setTextSize(18.0f);
 		        
+		        subject.setText("  " + currCourse.getName() + "  ");
+		        lettGrade.setText("  " + currCourse.getLetterGrade() + "  ");
+		        pctGrade.setText("  " + currCourse.getPercentGrade() + "  ");
+		        numZero.setText("  " + currCourse.getNumZeros() + "  ");
+
 		        row.addView(subject);
 		        row.addView(lettGrade);
 		        row.addView(pctGrade);
 		        row.addView(numZero);
 		        
-		        table.addView(row, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		        table.addView(row, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 			}
 		}
     };
