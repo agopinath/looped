39ccf52b0797306cf22681b31210b1f8765c9e50
Ajay Gopinath <ajgopi124@gmail.com>
2012-11-23 10:07:23 -0800

    finished news article listing in list view"

--- src/com/cyanojay/looped/portal/NewsActivity.java
+++ src/com/cyanojay/looped/portal/NewsActivity.java
@@ -1,22 +1,89 @@
 package com.cyanojay.looped.portal;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
+import java.util.List;
 
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.cyanojay.looped.API;
 import com.cyanojay.looped.R;
 
-public class NewsActivity extends Activity {
+public class NewsActivity extends ListActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        
+        ScrapeNewsTask task = new ScrapeNewsTask();
+        task.execute();
     }
+    
+    private class ScrapeNewsTask extends AsyncTask<String, Void, List<NewsArticle>> {
+    	@Override
+		protected List<NewsArticle> doInBackground(String... params) {
+			return API.get().getNews();
+		}
+		
+		@Override
+	    protected void onPostExecute(List<NewsArticle> result) {
+	        super.onPostExecute(result);
+	        
+	        NewsArticle[] values = result.toArray(new NewsArticle[0]);
+	        NewsAdapter adapter = new NewsAdapter(NewsActivity.this, values);
+	        
+	        NewsActivity.this.setListAdapter(adapter);
+		}
+    };
+    
+    private class NewsAdapter extends ArrayAdapter<NewsArticle> {
+    	  private final Context context;
+    	  private final NewsArticle[] values;
+    	  
+    	  public NewsAdapter(Context context, NewsArticle[] values) {
+    		  super(context, R.layout.curr_news_row, values);
+    		  this.context = context;
+    		  this.values = values;
+    	  }
 
+    	  @Override
+    	  public View getView(int position, View convertView, ViewGroup parent) {
+    		  LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    		  View rowView = inflater.inflate(R.layout.curr_news_row, parent, false);
+    		  
+    		  TextView articleName = (TextView) rowView.findViewById(R.id.news_post_title);
+    		  TextView articleAuthor = (TextView) rowView.findViewById(R.id.news_post_author);
+    		  TextView articleDate = (TextView) rowView.findViewById(R.id.news_post_date);
+    		  
+    		  NewsArticle article = values[position];
+    		  
+    		  articleName.setText(article.getArticleName());
+    		  articleAuthor.setText(article.getAuthor());
+    		  articleDate.setText(article.getDatePosted());
+    		  
+    		  return rowView;
+    	} 
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_news, menu);
         return true;
+    }
+    
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+    	NewsArticle item = (NewsArticle) getListAdapter().getItem(position);
+    	Toast.makeText(this, item.getArticleName() + " selected", Toast.LENGTH_SHORT).show();
     }
 }
