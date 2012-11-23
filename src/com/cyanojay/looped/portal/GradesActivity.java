079273e0b3dfb76ad8c201a0be520cbeb4ea0637
Ajay Gopinath <ajgopi124@gmail.com>
2012-11-22 17:30:55 -0800

    removing '# of zeroes' column due to weird glitch, will fix later

--- src/com/cyanojay/looped/portal/GradesActivity.java
+++ src/com/cyanojay/looped/portal/GradesActivity.java
@@ -43,7 +43,6 @@ public class GradesActivity extends Activity {
 	        TextView subj_head = new TextView(GradesActivity.this);
 	        TextView lett_head = new TextView(GradesActivity.this);
 	        TextView pct_head = new TextView(GradesActivity.this);
-	        TextView zero_head = new TextView(GradesActivity.this);
 	        
 	        subj_head.setTextSize(20.0f);
 	        subj_head.setText("  Course Name  \n");
@@ -51,13 +50,10 @@ public class GradesActivity extends Activity {
 	        lett_head.setText("  Letter Grade  \n");
 	        pct_head.setTextSize(20.0f);
 	        pct_head.setText("  % Grade  \n");
-	        zero_head.setTextSize(20.0f);
-	        zero_head.setText("  # of Zeroes  \n");
 	        
 	        headers.addView(subj_head);
 	        headers.addView(lett_head);
 	        headers.addView(pct_head);
-	        headers.addView(zero_head);
 	        
 	        table.addView(headers, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 	        
@@ -67,22 +63,18 @@ public class GradesActivity extends Activity {
 		        TextView subject = new TextView(GradesActivity.this);
 		        TextView lettGrade = new TextView(GradesActivity.this);
 		        TextView pctGrade = new TextView(GradesActivity.this);
-		        TextView numZero = new TextView(GradesActivity.this);
 		        
 		        subject.setTextSize(18.0f);
 		        lettGrade.setTextSize(18.0f);
 		        pctGrade.setTextSize(18.0f);
-		        numZero.setTextSize(18.0f);
 		        
 		        subject.setText("  " + currCourse.getName() + "  ");
 		        lettGrade.setText("  " + currCourse.getLetterGrade() + "  ");
 		        pctGrade.setText("  " + currCourse.getPercentGrade() + "  ");
-		        numZero.setText("  " + currCourse.getNumZeros() + "  ");
 
 		        row.addView(subject);
 		        row.addView(lettGrade);
 		        row.addView(pctGrade);
-		        row.addView(numZero);
 		        
 		        table.addView(row, new TableLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 			}
