4f053e7cacfcae5b1bbd93eadfa58bebfcbcb83e
Ajay Gopinath <ajgopi124@gmail.com>
2012-11-25 12:53:53 -0800

    finishing touches for v0.1

--- src/com/cyanojay/looped/portal/GradeDetailsActivity.java
+++ src/com/cyanojay/looped/portal/GradeDetailsActivity.java
@@ -29,7 +29,7 @@ public class GradeDetailsActivity extends BaseListActivity {
         Course currCourse = (Course) getIntent().getSerializableExtra("COURSE_SELECTED");
         
         setTitle(API.get().getPortalTitle());
-        getActionBar().setSubtitle(currCourse.getName() + " ~ " + 
+        getActionBar().setSubtitle(currCourse.getName() + " - " + 
         						   currCourse.getPercentGrade() + " " + currCourse.getLetterGrade());
         
         ScrapeGradeDetailsTask task = new ScrapeGradeDetailsTask();
--- src/com/cyanojay/looped/portal/NewsActivity.java
+++ src/com/cyanojay/looped/portal/NewsActivity.java
@@ -135,7 +135,7 @@ public class NewsActivity extends BaseListActivity {
 	        
 	        info.setText(Html.fromHtml(infoStr));
 	        
-	        final PopupWindow pw = new PopupWindow(flow, width-((int)(0.25*width)), height-((int)(0.4*height)), true);
+	        final PopupWindow pw = new PopupWindow(flow, width-((int)(0.25*width)), height-((int)(0.5*height)), true);
 	        
 	        wrapper.setOnClickListener(new View.OnClickListener() {
 	            @Override
