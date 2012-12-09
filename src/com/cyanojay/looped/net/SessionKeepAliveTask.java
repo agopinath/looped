44e9e4ef705b9884ba3eca4ea7188a8d5d601bfb
Ajay Gopinath <ajgopi124@gmail.com>
2012-12-09 11:31:47 -0800

    changed session keep alive interval to 10 mins

--- src/com/cyanojay/looped/net/SessionKeepAliveTask.java
+++ src/com/cyanojay/looped/net/SessionKeepAliveTask.java
@@ -5,11 +5,10 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.widget.Toast;
 
 public class SessionKeepAliveTask  {
-	private static final int UPDATE_INTERVAL = (int) (8 * 1000);
+	private static final int UPDATE_INTERVAL = (int) (60 * 1000 * 10);
 	private Timer timer = new Timer();
 	private Activity parent;  
 	
@@ -30,13 +29,15 @@ public class SessionKeepAliveTask  {
 		if(timer != null) timer.cancel();
 	}
 	
-	private void doKeepAlive() {
+	private boolean doKeepAlive() {
 		try {
-			System.out.println("RUNNING KEEP ALIVE");
 			API.get().refreshPortal();
 		} catch (IOException e) {
 			Toast.makeText(parent, "Failed to refresh School Loop. Please check your " +
 									"Internet connection and re-login.", Toast.LENGTH_LONG).show();
+			return false;
 		}
+		
+		return true;
 	}
 }
