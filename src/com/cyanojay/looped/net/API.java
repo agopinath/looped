1be183b34195b6817ae3a11662b665e085da5430
Ajay Gopinath <ajgopi124@gmail.com>
2012-12-20 19:01:04 -0800

    removing DebugMailer references

--- src/com/cyanojay/looped/net/API.java
+++ src/com/cyanojay/looped/net/API.java
@@ -173,27 +173,21 @@ public final class API {
 	    		if(isNotPublished) {
 	    			break;
 	    		}
-	    			
-	    		try {
-		    		if(i == 0) newCourse.setLetterGrade(currGrade.text());
-		    		if(i == 1) newCourse.setPercentGrade(currGrade.text());
-		    		if(i == 2 && currGrade.text().length() > 0) {
-		    			try {
-		    				newCourse.setNumZeros(Integer.parseInt(currGrade.text()));
-		    			} catch(NumberFormatException e) {
-		    				newCourse.setNumZeros(0);
-		    			}
+		    	if(i == 0) newCourse.setLetterGrade(currGrade.text());
+		    	if(i == 1) newCourse.setPercentGrade(currGrade.text());
+		    	if(i == 2 && currGrade.text().length() > 0) {
+		    		try {
+		    			newCourse.setNumZeros(Integer.parseInt(currGrade.text()));
+		    		} catch(NumberFormatException e) {
+		    			newCourse.setNumZeros(0);
 		    		}
-		    		if(i == 3) {
-		    			if(currGrade.children().hasAttr("href")) {
-			    			String detailsUrl = portalUrl + currGrade.children().attr("href");
-			    			newCourse.setDetailsUrl(detailsUrl);
-		    			}
+		    	}
+		    	if(i == 3) {
+		    		if(currGrade.children().hasAttr("href")) {
+		    			String detailsUrl = portalUrl + currGrade.children().attr("href");
+			    		newCourse.setDetailsUrl(detailsUrl);
 		    		}
-	    		} catch(Exception e) {
-	    			DebugMailer.sendDebugToDevs(e.getMessage());
-	    			continue;
-	    		}
+		    	}
 	    	}
 	    	
 	    	courses.add(newCourse);
@@ -209,7 +203,6 @@ public final class API {
 	    Elements assignmentsBlock = portal.body().select("tbody.hub_general_body tr");
 	    
 	    for(Element assignmentRow: assignmentsBlock) {
-	    	try {
 		    	// create new empty assignment to store course information
 		    	CurrentAssignment assignment = new CurrentAssignment();
 		    	
@@ -221,30 +214,20 @@ public final class API {
 		    	
 		    	for(int i = 0; i < details.size(); i++) {
 		    		Element currAssignment = details.get(i);
-		    		
-		    		try {
-			    		if(i == 0) {
-			    			if(currAssignment.children().hasAttr("href")) {
-				    			String detailsUrl = portalUrl + currAssignment.children().attr("href");
-				    			assignment.setDetailsUrl(detailsUrl);
-			    			}
-			    			assignment.setName(currAssignment.text());
+			    	if(i == 0) {
+			    		if(currAssignment.children().hasAttr("href")) {
+				    	String detailsUrl = portalUrl + currAssignment.children().attr("href");
+				    		assignment.setDetailsUrl(detailsUrl);
 			    		}
-			    		if(i == 1) assignment.setCourseName(currAssignment.text());
-			    		if(i == 2) assignment.setDueDate(currAssignment.text());
-		    		} catch(Exception e) {
-		    			DebugMailer.sendDebugToDevs(e.getMessage());
-		    			continue;
-		    		}
-		    	}
-		    	
-		    	assignments.add(assignment);
-	    	} catch(Exception e) {
-	    		DebugMailer.sendDebugToDevs(e.getMessage());
-	    		continue;
-	    	}
+			    		assignment.setName(currAssignment.text());
+			    	}
+			    	
+			    	if(i == 1) assignment.setCourseName(currAssignment.text());
+			    	if(i == 2) assignment.setDueDate(currAssignment.text());
+		    }
+		    
+		    assignments.add(assignment);
 	    }
-	    
 	    return assignments;
 	}
 	
@@ -260,44 +243,38 @@ public final class API {
 	    Elements articleInfo = newsBlock.select("td.list_text");
 	    
 	    for(int i = 0; i < articleNames.size(); i++) {
-	    	try {
-		    	// create new empty news article to store the article info
-		    	NewsArticle article = new NewsArticle();
+		    // create new empty news article to store the article info
+		    NewsArticle article = new NewsArticle();
+		    
+		    // select info according to how it is ordered/structured in the HTML
+		    Element title = articleNames.get(i);
+		    Element author = articleInfo.get(2*i);
+		    Element date = articleInfo.get((2*i)+1);
+		   	
+		    // split the author field into the author's name and the author's type
+		    String authorData[] = null;
 		    	
-		    	// select info according to how it is ordered/structured in the HTML
-		    	Element title = articleNames.get(i);
-		    	Element author = articleInfo.get(2*i);
-		    	Element date = articleInfo.get((2*i)+1);
+		   	try {
+		   		authorData = author.text().split(" - ");
+		   	} catch(Exception e) {
+		    	authorData = null;
+		    }
 		    	
-		    	// split the author field into the author's name and the author's type
-		    	String authorData[] = null;
-		    	
-		    	try {
-		    		authorData = author.text().split(" - ");
-		    	} catch(Exception e) {
-		    		DebugMailer.sendDebugToDevs(e.getMessage());
-		    		authorData = null;
-		    	}
-		    	
-		    	article.setArticleName(title.text());
-		    	
-		    	if(authorData != null) {
-			    	for(int j = 0; j < authorData.length; j++) {
-			    		if(j == 0) article.setAuthor(authorData[0]);
-			    		if(j == 1) article.setAuthorType(authorData[1]);
-			    	}
-		    	} else {
-		    		article.setDisplayAuthor(author.text());
-		    	}
-		    		
-		    	article.setDatePosted(date.text());
-		    	article.setArticleUrl(portalUrl + title.attr("href"));
-		    	
-		    	news.add(article);
-	    	} catch(Exception e) {
-	    		DebugMailer.sendDebugToDevs(e.getMessage());
-	    		continue;
-	    	}
+		    article.setArticleName(title.text());
+		    
+		    if(authorData != null) {
+			   	for(int j = 0; j < authorData.length; j++) {
+			   		if(j == 0) article.setAuthor(authorData[0]);
+			   		if(j == 1) article.setAuthorType(authorData[1]);
+			   	}
+		   	} else {
+		   		article.setDisplayAuthor(author.text());
+		   	}
+		   		
+		    article.setDatePosted(date.text());
+		   	article.setArticleUrl(portalUrl + title.attr("href"));
+		    
+		    news.add(article);
 	    }
 	    
 	    return news;
@@ -313,20 +290,15 @@ public final class API {
 		Elements mailListing = mailTable.select("tr:gt(0)");
 		
 		for(Element currListing : mailListing) {
-			try {
-				MailEntry currEntry = new MailEntry();
-				Elements mailInfo = currListing.select("td.list_text");
-				
-				currEntry.setTimestamp(mailInfo.get(0).text());
-				currEntry.setInvolvedParties(mailInfo.get(1).text());
-				currEntry.setSubject(mailInfo.get(2).text());
-				currEntry.setContentUrl(portalUrl + mailInfo.get(2).select("a").attr("href") + "&template=print");
-				
-				mail.add(currEntry);
-			} catch(Exception e) {
-				DebugMailer.sendDebugToDevs(e.getMessage());
-				continue;
-			}
+			MailEntry currEntry = new MailEntry();
+			Elements mailInfo = currListing.select("td.list_text");
+			
+			currEntry.setTimestamp(mailInfo.get(0).text());
+			currEntry.setInvolvedParties(mailInfo.get(1).text());
+			currEntry.setSubject(mailInfo.get(2).text());
+			currEntry.setContentUrl(portalUrl + mailInfo.get(2).select("a").attr("href") + "&template=print");
+			
+			mail.add(currEntry);
 		}
 		
 		return mail;
@@ -342,65 +314,55 @@ public final class API {
 	    Elements details = detailsPage.body().select("tbody.general_body tr");
 
 	    for(Element detail: details) {
-	    	try {
-		    	GradeDetail newDetail = new GradeDetail();
-		    	
-		    	// select all individual elements in each row
-		    	Elements data = detail.select("td");
-		    	
-		    	for(int i = 0; i < data.size(); i++) {
-		    		try {
-			    		if(i == 0) newDetail.setCategory(data.get(0).text());
-			    		if(i == 1) newDetail.setDueDate(data.get(1).text());
-			    		if(i == 2) newDetail.setDetailName(data.get(2).text());
-			    		if(i == 5) newDetail.setComment(data.get(5).text());
-			    		if(i == 6) newDetail.setSubmissions(data.get(6).text());
-		    		} catch(Exception e) {
-		    			DebugMailer.sendDebugToDevs(e.getMessage());
-						continue;
+		    GradeDetail newDetail = new GradeDetail();
+		    
+		    // select all individual elements in each row
+		    Elements data = detail.select("td");
+		    
+		    for(int i = 0; i < data.size(); i++) {
+			   	if(i == 0) newDetail.setCategory(data.get(0).text());
+			   	if(i == 1) newDetail.setDueDate(data.get(1).text());
+			   	if(i == 2) newDetail.setDetailName(data.get(2).text());
+			   	if(i == 5) newDetail.setComment(data.get(5).text());
+			   	if(i == 6) newDetail.setSubmissions(data.get(6).text());
+		    }
+		    
+			if (data.size() >= 5) {
+				String scoreData = data.get(4).text().trim();
+
+				// if the score is a numerical entry, split the grade into total
+				// points and earned points
+				if (scoreData.length() > 0 && Character.isDigit(scoreData.charAt(0))) {
+					String displayPct = scoreData.split(" = ")[1].trim();
+					newDetail.setDisplayPercent(displayPct);
+
+					// remove trailing '=' and percentage data
+					scoreData = scoreData.substring(0, scoreData.indexOf('='));
+
+					// split into respective parts and parse
+					String[] scoreParts = scoreData.split(" / ");
+
+					try {
+						newDetail.setPointsEarned(Double.parseDouble(scoreParts[0].trim()));
+						newDetail.setTotalPoints(Double.parseDouble(scoreParts[1].trim()));
+					} catch (NumberFormatException e) {
+						e.printStackTrace();
+						newDetail.setDisplayPercent(Constants.EMPTY_INDIC);
+						newDetail.setDisplayScore(Constants.EMPTY_INDIC);
 					}
-		    	}
-		    	
-		    	
-		    	if(data.size() >= 5) {
-			    	String scoreData = data.get(4).text().trim();
-			    	
-			    	// if the score is a numerical entry, split the grade into total points and earned points
-			    	if(scoreData.length() > 0 && Character.isDigit(scoreData.charAt(0))) {
-			    		String displayPct = scoreData.split(" = ")[1].trim();
-			    		newDetail.setDisplayPercent(displayPct);
-			    		
-			    		// remove trailing '=' and percentage data
-			    		scoreData = scoreData.substring(0, scoreData.indexOf('='));
-			    		
-			    		// split into respective parts and parse
-			    		String[] scoreParts = scoreData.split(" / ");
-			    		
-			    		try {
-				    		newDetail.setPointsEarned(Double.parseDouble(scoreParts[0].trim()));
-				    		newDetail.setTotalPoints(Double.parseDouble(scoreParts[1].trim()));
-			    		} catch(NumberFormatException e) {
-			    			DebugMailer.sendDebugToDevs(e.getMessage());
-			    			newDetail.setDisplayPercent(Constants.EMPTY_INDIC);
-				    		newDetail.setDisplayScore(Constants.EMPTY_INDIC);
-			    		}
-			    		
-			    		String displayScore = scoreData.replaceAll(" ", "");
-			    		newDetail.setDisplayScore(displayScore);
-			    	} else {
-			    		newDetail.setDisplayPercent(Constants.EMPTY_INDIC);
-			    		newDetail.setDisplayScore(Constants.EMPTY_INDIC);
-			    	}
-		    	} else {
-			    	newDetail.setDisplayPercent(Constants.EMPTY_INDIC);
-		    		newDetail.setDisplayScore(Constants.EMPTY_INDIC);
-			    }
-		    	
-			    detailsList.add(newDetail);
-	    	} catch(Exception e) {
-	    		DebugMailer.sendDebugToDevs(e.getMessage());
-				continue;
+
+					String displayScore = scoreData.replaceAll(" ", "");
+					newDetail.setDisplayScore(displayScore);
+				} else {
+					newDetail.setDisplayPercent(Constants.EMPTY_INDIC);
+					newDetail.setDisplayScore(Constants.EMPTY_INDIC);
+				}
+			} else {
+				newDetail.setDisplayPercent(Constants.EMPTY_INDIC);
+				newDetail.setDisplayScore(Constants.EMPTY_INDIC);
 			}
+		    
+		    detailsList.add(newDetail);
 	    }
 	    
 	    return detailsList;
