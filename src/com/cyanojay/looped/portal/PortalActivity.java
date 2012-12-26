b5683cf6cb766520d2360c39bd487b660362a1d4
Ajay Gopinath <ajgopi124@gmail.com>
2012-12-26 13:20:28 -0800

    got basic swiping to work with test Fragments, need to port to actual Activities

--- src/com/cyanojay/looped/portal/PortalActivity.java
+++ src/com/cyanojay/looped/portal/PortalActivity.java
@@ -1,36 +1,41 @@
 package com.cyanojay.looped.portal;
 
-import android.app.TabActivity;
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.text.Html;
 import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.cyanojay.looped.MainActivity;
 import com.cyanojay.looped.R;
 import com.cyanojay.looped.Utils;
 import com.cyanojay.looped.net.API;
-import com.cyanojay.looped.net.KeepAliveService;
 
-public class PortalActivity extends TabActivity {
-	private TabHost tabHost;
+public class PortalActivity extends SherlockFragmentActivity {
 	public static Intent KEEP_ALIVE_TASK;
 	
-    @Override
+	private TabsAdapter adapter;
+	private ViewPager mPager;
+	
+   /* @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portal);
@@ -46,48 +51,124 @@ public class PortalActivity extends TabActivity {
         
         KEEP_ALIVE_TASK = new Intent(this, KeepAliveService.class);
         startService(KEEP_ALIVE_TASK);
-	}
+	}*/
     
-	private void setupTab(final String tag, Intent e) {
-	    View tabview = createTabView(tabHost.getContext(), tag);
-	    TabSpec tabSpec = tabHost.newTabSpec(tag).setIndicator(tabview).setContent(e);
-
-	    tabHost.addTab(tabSpec);
-	}
-	
-	private static View createTabView(final Context context, final String text) {
-	    View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
-	    TextView tv = (TextView) view.findViewById(R.id.tabsText);
-	    tv.setMaxLines(1);
-	    tv.setText(text);
-	    return view;
-	}
-	
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar bar = getSupportActionBar();
+        
+        mPager = new ViewPager(this);
+        mPager.setId(R.id.pager);
+        
+        setContentView(mPager);
+        
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
+
+        adapter = new TabsAdapter(this, mPager);
+        
+        setupTab(bar, "Grades", TestFragment.class);
+        setupTab(bar, "Assignments", TestFragment.class);
+        setupTab(bar, "News", TestFragment.class);
+        setupTab(bar, "Mail", TestFragment.class);
+    }
+    
+	private void setupTab(ActionBar actionBar, String text, Class<?> fragmentClass) {
+		adapter.addTab(actionBar.newTab().setText(text), fragmentClass, null);  
+	}
+	
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_portal, menu);
         return true;
-    }
+    }*/
     
-    @Override
+   /* @Override
     public void onBackPressed() {
     	logOut();
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_about:
-                showAbout();
-                return true;
-            case R.id.menu_logout:
-                logOut();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    
+    }*/
+
+	public static class TabsAdapter extends FragmentPagerAdapter implements
+											ActionBar.TabListener, ViewPager.OnPageChangeListener {
+		private final Context mContext;
+		private final ActionBar mActionBar;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+		static final class TabInfo {
+			private final Class<?> clss;
+			private final Bundle args;
+
+			TabInfo(Class<?> _class, Bundle _args) {
+				clss = _class;
+				args = _args;
+			}
+		}
+
+		public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager) {
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mActionBar = activity.getSupportActionBar();
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
+
+		public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
+			TabInfo info = new TabInfo(clss, args);
+			tab.setTag(info);
+			tab.setTabListener(this);
+			mTabs.add(info);
+			mActionBar.addTab(tab);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(),
+					info.args);
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset,
+				int positionOffsetPixels) {
+		}
+
+		@Override
+		public void onPageSelected(int position) {
+			mActionBar.setSelectedNavigationItem(position);
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			Object tag = tab.getTag();
+			for (int i = 0; i < mTabs.size(); i++) {
+				if (mTabs.get(i) == tag) {
+					mViewPager.setCurrentItem(i);
+				}
+			}
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		}
+	}
+	
     private void logOut() {
     	if(Utils.isOnline(this)) {
     		API.get().logOut();
