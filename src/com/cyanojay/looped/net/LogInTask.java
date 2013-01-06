d875d2762633579d1b581e8e69351d65bb76636e
Ajay Gopinath <ajgopi124@gmail.com>
2013-01-06 14:59:57 -0800

    temporarily removing login ensure

--- src/com/cyanojay/looped/net/LogInTask.java
+++ src/com/cyanojay/looped/net/LogInTask.java
@@ -88,7 +88,7 @@ public class LogInTask extends AsyncTask<String, String, LogInTask.LoginStatus> 
 	        case LOGIN_SUCCESS:
 	        	//System.out.println("\n\nLOG IN SUCCESS\n\n");
 	        	
-	        	new Thread(new Runnable() {
+	        	/*new Thread(new Runnable() {
 					@Override
 					public void run() {
 						try {
@@ -97,7 +97,7 @@ public class LogInTask extends AsyncTask<String, String, LogInTask.LoginStatus> 
 							e.printStackTrace();
 						}
 					}
-	        	}).start();
+	        	}).start();*/
 	        	
 	        	Intent showPortalIntent = new Intent(parent, PortalActivity.class);
 	        	parent.startActivity(showPortalIntent);
